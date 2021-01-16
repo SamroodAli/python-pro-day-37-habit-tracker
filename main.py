417d10365ba061db3a73a05893a4ac96e09a4bd5
SamroodAli <samroodali@live.com>
2021-01-16 15:19:45 +0530

    API:Adding new value to the graph

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Pixela Habit Tracker Main Module"""
 import os
 import requests
-
+import datetime as dt
 # Environmental variables - pixela token and username, they are arbitrary token and username.
 pixela_token = os.environ.get("token")
 pixela_username = os.environ.get("username")
@@ -14,7 +14,14 @@ GRAPH_UNIT = "Hour"
 GRAPH_UNIT_TYPE = "float"
 GRAPH_COLOR = "ajisai"
 
+# Today's value- No of graph units in string format
+QUANTITY_TODAY = "3"
 
+# Posting a value to the today
+date = str(dt.datetime.now().strftime("%Y%m%d"))
+# uncomment the line below for a custom date
+# date = str(dt.datetime(year=2021, month=1, day=15).strftime("%Y%m%d"))
+print(date)
 user_params ={
     "token": pixela_token,
     "username": pixela_username,
@@ -40,6 +47,15 @@ headers ={
 # Creating a graph- a one time request
 graph_response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
 
-# To view your graph url in the browser - requesting html
-graph_url = f"{GRAPH_ENDPOINT}/{GRAPH_ID}.html"
-print(f"Here is your graph link:\n{graph_url}")
+# To view your graph url in the browser or update a value in it
+graph_url = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
+
+# Graph value configuration
+value_config = {
+    "date": date,
+    "quantity": QUANTITY_TODAY,
+}
+
+# Add value to the graph
+add_value_response = requests.post(url=graph_url, headers=headers, json=value_config)
+print(add_value_response.text)
