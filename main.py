4f1f4ee4b4b9a7665376bae0cca0d8e7c9eff904
SamroodAli <samroodali@live.com>
2021-01-16 15:24:56 +0530

    Readability:Pylint fixes

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 """Pixela Habit Tracker Main Module"""
 import os
-import requests
 import datetime as dt
+import requests
+
 # Environmental variables - pixela token and username, they are arbitrary token and username.
+# replace the os.environ.get values by your own token and username
+# token example - sdasd12kjn3
+# name example -"john" - all in small letters
+
 pixela_token = os.environ.get("token")
 pixela_username = os.environ.get("username")
-PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 # Graph data
 GRAPH_ID = "graph1"
@@ -14,14 +18,17 @@ GRAPH_UNIT = "Hour"
 GRAPH_UNIT_TYPE = "float"
 GRAPH_COLOR = "ajisai"
 
-# Today's value- No of graph units in string format
+# Posting a value to the today
+# DATE = str(dt.datetime.now().strftime("%Y%m%d"))
+# uncomment the line below for a custom date
+DATE = str(dt.datetime(year=2021, month=1, day=14).strftime("%Y%m%d"))
+
+# Today's value- No of graph units in string format for the date, 3 is an example
 QUANTITY_TODAY = "3"
 
-# Posting a value to the today
-date = str(dt.datetime.now().strftime("%Y%m%d"))
-# uncomment the line below for a custom date
-# date = str(dt.datetime(year=2021, month=1, day=15).strftime("%Y%m%d"))
-print(date)
+# Pixela endpoint
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+
 user_params ={
     "token": pixela_token,
     "username": pixela_username,
@@ -52,10 +59,9 @@ graph_url = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
 
 # Graph value configuration
 value_config = {
-    "date": date,
+    "date": DATE,
     "quantity": QUANTITY_TODAY,
 }
 
 # Add value to the graph
 add_value_response = requests.post(url=graph_url, headers=headers, json=value_config)
-print(add_value_response.text)
