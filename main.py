5b4c5b895e3aca155943e9c42fde8f4d36a2e273
SamroodAli <samroodali@live.com>
2021-01-16 15:38:45 +0530

    Fix:change create graph to create user

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ GRAPH_UNIT_TYPE = "float"
 GRAPH_COLOR = "ajisai"
 
 # Posting a value to the today
-# DATE = str(dt.datetime.now().strftime("%Y%m%d"))
+DATE = str(dt.datetime.now().strftime("%Y%m%d"))
 # uncomment the line below for a custom date
-DATE = str(dt.datetime(year=2021, month=1, day=14).strftime("%Y%m%d"))
+# DATE = str(dt.datetime(year=2021, month=1, day=14).strftime("%Y%m%d"))
 
 # Today's value- No of graph units in string format for the date, 3 is an example
 QUANTITY_TODAY = "3"
@@ -35,8 +35,8 @@ user_params ={
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-
+create_user_response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+print(create_user_response.text)
 # Graph data
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{pixela_username}/graphs"
 
@@ -53,7 +53,7 @@ headers ={
 }
 # Creating a graph- a one time request
 graph_response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
-
+print(graph_response.text)
 # To view your graph url in the browser or update a value in it
 graph_url = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
 
@@ -65,3 +65,19 @@ value_config = {
 
 # Add value to the graph
 add_value_response = requests.post(url=graph_url, headers=headers, json=value_config)
+print(add_value_response.text)
+
+# delete a value- change the date here
+DELETE_DATE = str(dt.datetime(year=2021, month=1, day=16).strftime("%Y%m%d"))
+
+
+# Deleting value function
+def delete_value():
+    """function to delete a pixel/value from the graph"""
+    endpoint = f"{graph_url}/{DELETE_DATE}"
+    delete_response = requests.delete(url=endpoint, headers=headers)
+    print(delete_response.text)
+
+
+# Uncomment to delete
+delete_value()
