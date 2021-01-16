62bee5d1a68e210fbdc73b342138ef16fa7a0f88
SamroodAli <samroodali@live.com>
2021-01-16 13:45:32 +0530

    API:create a pixela account using http post

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ pixela_token = os.environ.get("token")
 pixela_username = os.environ.get("username")
 pixela_endpoint = "https://pixe.la/v1/users"
 
+user_params ={
+    "token": pixela_token,
+    "username": pixela_username,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+response = requests.post(url=pixela_endpoint, json=user_params)
